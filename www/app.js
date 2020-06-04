37a3b22af8f0f8700b455c8da32c458e4fe3de0e
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-06-04 17:24:42 -0400

    update www/app.js from generated plugin demo

--- www/app.js
+++ www/app.js
@@ -1,12 +1,14 @@
 // Copyright 2020-present Christopher J. Brody <chris.brody+brodybits@gmail.com>
 
-document
-  .getElementById('status')
-  .appendChild(document.createElement('b'))
-  .appendChild(document.createTextNode('status:'))
 document.addEventListener('deviceready', function () {
   document
     .getElementById('status')
-    .appendChild(document.createElement('i'))
+    .appendChild(document.createElement('b'))
     .appendChild(document.createTextNode('ready'))
+  window.bigResponsePayloadTest.test(function () {
+    document
+      .getElementById('status')
+      .appendChild(document.createElement('i'))
+      .appendChild(document.createTextNode('pong'))
+  })
 })
