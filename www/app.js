20ed5d0a168515813e78b18aaef9a0202112cdec
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-06-04 20:16:42 -0400

    update test app log messages

--- www/app.js
+++ www/app.js
@@ -1,14 +1,21 @@
 // Copyright 2020-present Christopher J. Brody <chris.brody+brodybits@gmail.com>
 
-document.addEventListener('deviceready', function () {
+function log (text) {
+  console.log(text)
+
   document
-    .getElementById('status')
-    .appendChild(document.createElement('b'))
-    .appendChild(document.createTextNode('ready'))
+    .getElementById('messages')
+    .appendChild(document.createTextNode(text))
+
+  document
+    .getElementById('messages')
+    .appendChild(document.createElement('br'))
+}
+
+document.addEventListener('deviceready', function () {
+  log('ready')
+
   window.bigResponsePayloadTest.test(function () {
-    document
-      .getElementById('status')
-      .appendChild(document.createElement('i'))
-      .appendChild(document.createTextNode('pong'))
+    log('done')
   })
 })
